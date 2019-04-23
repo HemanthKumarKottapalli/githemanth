faf658a615cb811a5a24b0b31d702fa1e4250515
nagraju <nagaraju.j008@gmail.com>
2019-04-23 15:09:02 +0530

    main method and print line added

--- Sample.java
+++
@@ -1,6 +0,0 @@
-class Sample
-{
-public static void(String argus[])
-{system.out.println("welcome to git");
-}
-}
--- SampleGit.java
+++
@@ -1,9 +0,0 @@
- class SampleGit
- {
-   public static void main(String args[])
-    
-    {
-		System.out.println("Welcome to the GIT platform");
-	}
-
- }
